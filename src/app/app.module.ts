4e1eba1c9df94ebd8e328ff3773e1095166c3411
Sergio Flores <sergioflores.vt@gmail.com>
2023-08-17 18:28:36 -0300

    Agregado de tablas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,8 +31,7 @@ import { GraficosComponent } from './shared/graficos/graficos.component';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule,
-   
-  
+    FormsModule
    
   ],
   providers: [],
--- src/app/shared/graficos/graficos.component.ts
+++ src/app/shared/graficos/graficos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Chart, registerables } from 'chart.js';
 
 Chart.register(...registerables)
@@ -8,27 +8,17 @@ Chart.register(...registerables)
   styleUrls: ['./graficos.component.css']
 })
 export class GraficosComponent implements OnInit{
+
+  
   ngOnInit(): void {
-    this.RenderChart()
-  }
-  RenderChart(){
-    new Chart('piechart', {
-      type: 'doughnut',
-      data: {
-        labels: ['Red'+ ' 20%', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [20, 19, 3, 5, 2, 3],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
-  }
+  
+
 }
+}
+  
+
+  // updateChart(){
+  //   console.log('Botón de actualizar clicado')
+  //   this.RenderChart
+  // }
+
